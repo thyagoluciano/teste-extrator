0e2d839a2fb9cf7c68910623eb199506587bf9ef
thyagoluciano <thyagoluciano@gmail.com>
2021-08-30 18:03:32 -0300

    teste

--- pages/api/teste/index.ts
+++ pages/api/teste/index.ts
@@ -1,5 +1,10 @@
 export default function handler(req, res) {
     console.log(req.headers);
-    res.status(200).json({headers:req.headers})
+    const headers = req.headers;
+    
+
+    headers.hasOwnProperty('x-teste-slug');
+
+    res.status(200).json({"teste": headers['x-teste-slug']})
 }
   
